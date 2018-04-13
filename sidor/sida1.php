ddfa682181415067a63e4dbc7a5ac3fc39ff6587
WilliamMoller <william.moller.te16d@gmail.com>
2018-04-13 10:05:56 +0200

    html
    fixat html och css.

---
+++ sidor/sida1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dawei</title>
+    <link rel="stylesheet" href="../css/sida1.css">
+</head>
+<body>
+
+    <header class="header">
+        <a href="../sidor/sida1.php" class="hem">Hem</a>
+        <h1 id="Dawei">Dawei</h1>
+        <a href="../sidor/sida2.php" class="bokning">Bokning</a>
+    </header>
+
+
+    <section class="parallax">
+        <div class="parallax-inner">
+            
+        </div>
+    </section>
+
+</body>
+
+</html>
